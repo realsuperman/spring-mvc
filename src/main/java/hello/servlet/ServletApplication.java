1375cf97cd73dd474cdd20e3c0ed10717eae482e
최성훈 <seonghun7304@naver.com>
2022-07-20 18:31:41 +0900

    초기 프로젝트 설정

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan // 서블릿 자동 등록(스프링의 컴포넌트 스캔이랑 유사함)
 @SpringBootApplication
 public class ServletApplication {
 
